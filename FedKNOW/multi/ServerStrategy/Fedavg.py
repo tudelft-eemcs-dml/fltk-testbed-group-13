fe74db906a9315181fc8826183b3b13338bbebc1
adis98 <aditya.ssr@gmail.com>
2022-06-13 00:00:20 +0200

    FedKNOW/multi/main_WEIT.py

--- FedKNOW/multi/ServerStrategy/Fedavg.py
+++ FedKNOW/multi/ServerStrategy/Fedavg.py
@@ -10,6 +10,7 @@ import time
 import sys
 import pandas as pd
 import csv
+import gzip
 
 from flwr.common import (
     EvaluateIns,
@@ -88,6 +89,7 @@ class OurFed(fl.server.strategy.FedAvg):
                     internal.append(self.kb[client][tensor])
                 kb_converted.append(np.stack(internal,axis=-1))
             kb_converted_string = pickle.dumps(kb_converted)
+            kb_converted_string = gzip.compress(kb_converted_string)
             self.kb = []
         config['kb'] = kb_converted_string
 
@@ -172,7 +174,7 @@ class OurFed(fl.server.strategy.FedAvg):
         num_clients = 0
         for _,fitres in results:
             if (fitres.metrics['kb'] != ""):
-                kb.append(pickle.loads(fitres.metrics['kb']))
+                kb.append(pickle.loads(gzip.decompress(fitres.metrics['kb'])))
             avg_client_parameter_size += fitres.metrics['parameter_size']
             avg_client_config_size += fitres.metrics['kb_size']
             avg_client_exec_time += fitres.metrics['clientExecTime']
