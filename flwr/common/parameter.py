5935956c7f7a3701f87c3737382041cdbda36dcd
rmadhwal <rohan@madhwal.com>
2022-06-15 01:29:34 +0200

    use blosc

--- flwr/common/parameter.py
+++ flwr/common/parameter.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 """Parameter conversion."""
-import gzip
+import blosc
 import pickle
 from io import BytesIO
 from typing import cast
@@ -25,16 +25,16 @@ from .typing import Parameters, Weights
 
 def weights_to_parameters(weights: Weights) -> Parameters:
     """Convert NumPy weights to parameters object."""
-    tensors = pickle.dumps([ndarray_to_bytes(ndarray) for ndarray in weights])
-    compressed_tensors = gzip.compress(tensors)
+    tensors = pickle.dumps(weights)
+    compressed_tensors = blosc.compress(tensors)
     return Parameters(tensors=compressed_tensors, tensor_type="numpy.ndarray")
 
 
 def parameters_to_weights(parameters: Parameters) -> Weights:
     """Convert parameters object to NumPy weights."""
-    decompressed_tensors = gzip.decompress(parameters.tensors)
-    tensors = pickle.loads(decompressed_tensors)
-    return [bytes_to_ndarray(tensor) for tensor in tensors]
+    decompressed_tensors = blosc.decompress(parameters.tensors)
+    weights = pickle.loads(decompressed_tensors)
+    return weights
 
 
 def ndarray_to_bytes(ndarray: np.ndarray) -> bytes:
